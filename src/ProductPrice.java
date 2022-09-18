0beb7ae178ea88d7ca985904d4cabc5741614798
Denis <denistynkovan5@gmail.com>
2022-09-18 16:38:17 +0300

    Create method for search a price product

--- src/ProductPrice.java
+++ src/ProductPrice.java
@@ -1,4 +1,10 @@
 public class ProductPrice extends Product {
 
+    public ProductPrice(String name, double quantity, double price) {
+        super(name, quantity, price);
+    }
 
+    public double productPrice(double quantity, double price){
+        return quantity * price;
+    }
 }
