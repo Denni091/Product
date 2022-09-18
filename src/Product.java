7aeef6272227359cca4ce6fd8abcb082a3c0c14d
Denis <denistynkovan5@gmail.com>
2022-09-18 16:34:08 +0300

    Create constructor, getter and setter for future work!

---
+++ src/Product.java
@@ -0,0 +1,33 @@
+public class Product {
+
+    private String name;
+    private double quantity;
+    private double price;
+
+    public Product(){
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(double quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
