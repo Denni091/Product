29d6282ea223f44303bbb944b179e9c60cd98cfe
Denis <denistynkovan5@gmail.com>
2022-09-18 17:01:42 +0300

    Create class for user and output cheque

---
+++ src/App.java
@@ -0,0 +1,61 @@
+
+import java.util.Scanner;
+
+public class App {
+
+    static String name;
+    static double quantity;
+    static double price;
+    static double priceProduct;
+    static double discount;
+    static double discountProduct;
+    static Scanner scanner;
+
+    public static void main(String[] args) {
+        setInfo();
+        setScanner();
+        calcProduct();
+        cheque();
+    }
+
+    public static void setInfo() {
+
+        System.out.println("Good evening in our shop!Please write product information"
+                + "\n---------------------------------------------------------");
+    }
+
+    public static void setScanner() {
+
+        scanner = new Scanner(System.in);
+        System.out.print("Write product name: ");
+        name = scanner.nextLine();
+
+        System.out.print("Write " + name + " quantity: ");
+        quantity = scanner.nextDouble();
+
+        System.out.print("Write " + name + " price: ");
+        price = scanner.nextDouble();
+
+        System.out.println("Write " + name + " discount: ");
+        discount = scanner.nextDouble();
+    }
+
+    public static void calcProduct() {
+
+        ProductPrice productPrice = new ProductPrice();
+        priceProduct = productPrice.productPrice(quantity, price);
+
+        ProductDiscount productDiscount = new ProductDiscount();
+        discountProduct = productDiscount.discount(quantity, price, discount);
+    }
+
+    public static void cheque(){
+
+        System.out.println("---------------------------------------------------------"
+                + "Product name: " + name
+                + "Product price: " + priceProduct
+                + "Discount: " + discountProduct
+                + "\n---------------------------------------------------------"
+                + "\nHave a nice day :)");
+    }
+}
--- src/ProductDiscount.java
+++ src/ProductDiscount.java
@@ -2,6 +2,9 @@ public class ProductDiscount extends ProductPrice {
 
     private double discount;
 
+    public ProductDiscount() {
+    }
+
     public ProductDiscount(String name, double quantity, double price, double discount) {
         super(name, quantity, price);
         this.discount = discount;
